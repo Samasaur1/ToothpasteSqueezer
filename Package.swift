875d8b2e3feae17d99a3b8e5eeaf3247394633ad
Sam <samgauck@icloud.com>
2020-07-03 12:24:32 -0400

    Convert to new DSL API

--- Package.swift
+++ Package.swift
@@ -1,15 +1,18 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.2
 // Managed by ice
 
 import PackageDescription
 
 let package = Package(
     name: "ToothpasteSqueezer",
+    platforms: [
+        .macOS(.v10_15)
+    ],
     products: [
         .executable(name: "ToothpasteSqueezer", targets: ["ToothpasteSqueezer"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Samasaur1/SwiftySCADKit", .branch("master")),
+        .package(url: "https://github.com/Samasaur1/SwiftySCADKit", .branch("dsl")),
     ],
     targets: [
         .target(name: "ToothpasteSqueezer", dependencies: ["SwiftySCADKit"]),
--- Sources/ToothpasteSqueezer/main.swift
+++ Sources/ToothpasteSqueezer/main.swift
@@ -1,8 +1,4 @@
 import SwiftySCADKit
-var main: OpenSCAD = OpenSCAD("")
-defer {
-    OPENSCAD_print(main)
-}
 
 var args: [Double] = []
 for argument in CommandLine.arguments.dropFirst() {
@@ -13,69 +9,200 @@ for argument in CommandLine.arguments.dropFirst() {
 
 print("Input gap")
 let gapDepth: Double = args.first != nil ? args.removeFirst() : Double(readLine() ?? "0.15") ?? 0.15
-
 print("Input slope horizontal distance (distance from hole projection to slope)")
 let slopeHorizontalDistance: Double = args.first != nil ? args.removeFirst() : Double(readLine() ?? "0.15") ?? 0.15
-
 print("Input toothpaste width (Including \"wiggle room\", i.e. don't just measure it, add some space)")
 let toothpasteWidth: Double = args.first != nil ? args.removeFirst() : Double(readLine() ?? "6.0") ?? 6.0//Including "wiggle room", i.e. don't just measure it, add some space.
-
-main = OpenSCAD.rectangularPrism(height: 3, width: toothpasteWidth, depth: gapDepth, centered: false)
-let slope = OpenSCAD.hexahedron(bottom: (.init(-slopeHorizontalDistance, -slopeHorizontalDistance, 0), .init(-slopeHorizontalDistance, gapDepth + slopeHorizontalDistance, 0), .init(toothpasteWidth + slopeHorizontalDistance, gapDepth + slopeHorizontalDistance, 0), .init(toothpasteWidth + slopeHorizontalDistance, -slopeHorizontalDistance, 0)), top: (.init(0, 0, 1), .init(0, gapDepth, 1), .init(toothpasteWidth, gapDepth, 1), .init(toothpasteWidth, 0, 1)))
-main.union(with: slope)
-
 print("Input padding")
 let padding: Double = args.first != nil ? args.removeFirst() : Double(readLine() ?? "0.25") ?? 0.25
-
-let cutoutHeight = 5.0 + (padding * 2) // so that extreme padding values still have the full cutout
-var cutout = OpenSCAD.cylinder(height: cutoutHeight, topRadius: 2, bottomRadius: 2, centered: true)
-cutout.rotate(by: 90, 0, 0)
-cutout.translate(by: 2, cutoutHeight/2, 2)
-cutout.scale(to: 3/4, 1, 1)
-cutout.translate(by: (toothpasteWidth/2)-1.5, (gapDepth / 2)-(cutoutHeight/2), 3-2)
-cutout.translate(by: 0, 0, 0.1) //so that the slope and cutout don't touch
-main.union(with: cutout)
-main.translate(by: slopeHorizontalDistance, slopeHorizontalDistance, 0)
-
-let dimensions = (height: 3.0, width: toothpasteWidth + (slopeHorizontalDistance * 2) + (padding * 2), depth: (slopeHorizontalDistance * 2) + gapDepth + (padding * 2))
-var minuend = OpenSCAD.rectangularPrism(height: dimensions.height, width: dimensions.width, depth: dimensions.depth, centered: false)
-minuend.translate(by: -padding, -padding, 0)
-main.difference(from: minuend)
-main.translate(by: padding, padding, 0)
-
 print("Input corner rounding (radius of the cylinder used to round)")
 let cornerRoundingRadius: Double = args.first != nil ? args.removeFirst() : Double(readLine() ?? "0.2") ?? 0.2
 
-var verticalCornerRounder = OpenSCAD.cylinder(height: dimensions.height, topRadius: cornerRoundingRadius, bottomRadius: cornerRoundingRadius, centered: false)
-verticalCornerRounder.translate(by: cornerRoundingRadius, cornerRoundingRadius, 0)
-verticalCornerRounder.difference(from: .rectangularPrism(height: dimensions.height, width: cornerRoundingRadius, depth: cornerRoundingRadius, centered: false))
-verticalCornerRounder.union(with: verticalCornerRounder.mirrored(across: 1, 0, 0).translated(by: dimensions.width, 0, 0))
-verticalCornerRounder.union(with: verticalCornerRounder.mirrored(across: 0, 1, 0).translated(by: 0, dimensions.depth, 0))
+let cutoutHeight = 1 + gapDepth + (padding * 2) + (slopeHorizontalDistance * 2) // so that extreme padding values still have the full cutout
+let dimensions = (height: 3.0, width: toothpasteWidth + (slopeHorizontalDistance * 2) + (padding * 2), depth: (slopeHorizontalDistance * 2) + gapDepth + (padding * 2))
 
-var widthCornerRounder = OpenSCAD.cylinder(height: dimensions.width, topRadius: cornerRoundingRadius, bottomRadius: cornerRoundingRadius, centered: false)
-widthCornerRounder.translate(by: cornerRoundingRadius, cornerRoundingRadius, 0)
-widthCornerRounder.difference(from: .rectangularPrism(height: dimensions.width, width: cornerRoundingRadius, depth: cornerRoundingRadius, centered: false))
-widthCornerRounder.union(with: widthCornerRounder.mirrored(across: 1, 0, 0).translated(by: dimensions.height, 0, 0))
-widthCornerRounder.union(with: widthCornerRounder.mirrored(across: 0, 1, 0).translated(by: 0, dimensions.depth, 0))
-widthCornerRounder.rotate(by: 0, 90, 0)
-widthCornerRounder.translate(by: 0, 0, dimensions.height)
+struct ToothpasteSqueezer: OpenSCAD {
+    var body: some OpenSCAD {
+        Difference(parent: {
+            Translate(padding, padding, 0) {
+                Difference(parent: {
+                    Translate(-padding, -padding, 0) {
+                        RectangularPrism(height: dimensions.height, width: dimensions.width, depth: dimensions.depth, centered: false)
+                    }
+                }) {
+                    inside
+                }
+            }
+        }) {
+            Union {
+                VerticalCornerRounder()
+                WidthCornerRounder()
+                DepthCornerRounder()
+                GripCornerRounder()
+            }
+        }
+    }
 
-var depthCornerRounder = OpenSCAD.cylinder(height: dimensions.depth, topRadius: cornerRoundingRadius, bottomRadius: cornerRoundingRadius, centered: false)
-depthCornerRounder.translate(by: cornerRoundingRadius, cornerRoundingRadius, 0)
-depthCornerRounder.difference(from: .rectangularPrism(height: dimensions.depth, width: cornerRoundingRadius, depth: cornerRoundingRadius, centered: false))
-depthCornerRounder.union(with: depthCornerRounder.mirrored(across: 1, 0, 0).translated(by: dimensions.width, 0, 0))
-depthCornerRounder.union(with: depthCornerRounder.mirrored(across: 0, 1, 0).translated(by: 0, dimensions.height, 0))
-depthCornerRounder.rotate(by: -90, 0, 0)
-depthCornerRounder.translate(by: 0, 0, dimensions.height)
+    var inside: some OpenSCAD {
+        Translate(slopeHorizontalDistance, slopeHorizontalDistance, 0) {
+            Union {
+                Union {
+                    RectangularPrism(height: 3, width: toothpasteWidth, depth: gapDepth, centered: false)
+                    Hexahedron(bottom: (.init(-slopeHorizontalDistance, -slopeHorizontalDistance, 0), .init(-slopeHorizontalDistance, gapDepth + slopeHorizontalDistance, 0), .init(toothpasteWidth + slopeHorizontalDistance, gapDepth + slopeHorizontalDistance, 0), .init(toothpasteWidth + slopeHorizontalDistance, -slopeHorizontalDistance, 0)), top: (.init(0, 0, 1), .init(0, gapDepth, 1), .init(toothpasteWidth, gapDepth, 1), .init(toothpasteWidth, 0, 1)))
+                }
+                cutout
+            }
+        }
+    }
 
-var gripCornerRounder = OpenSCAD.cylinder(height: dimensions.depth, topRadius: cornerRoundingRadius, bottomRadius: cornerRoundingRadius, centered: false)
-gripCornerRounder.translate(by: cornerRoundingRadius, cornerRoundingRadius, 0)
-gripCornerRounder.difference(from: .rectangularPrism(height: dimensions.depth, width: cornerRoundingRadius, depth: cornerRoundingRadius, centered: false))
-gripCornerRounder.union(with: gripCornerRounder.mirrored(across: 1, 0, 0).translated(by: -3 + (0.25 * cornerRoundingRadius), 0, 0))
-gripCornerRounder.rotate(by: -90, 0, 0)
-gripCornerRounder.translate(by: padding + (toothpasteWidth / 2) + slopeHorizontalDistance + 1.5 - (0.125 * cornerRoundingRadius), 0, dimensions.height)
+    var cutout: some OpenSCAD {
+        Translate(0, 0, 0.1) { //so that the slope and cutout don't touch
+            Translate((toothpasteWidth/2)-1.5, (gapDepth / 2)-(cutoutHeight/2), 3-2) {
+                Scale(3/4, 1, 1) {
+                    Translate(2, cutoutHeight/2, 2) {
+                        Rotate(90, 0, 0) {
+                            Cylinder(height: cutoutHeight, topRadius: 2, bottomRadius: 2, centered: true)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
 
-main = verticalCornerRounder.differenced(from: main)
-main = widthCornerRounder.differenced(from: main)
-main = depthCornerRounder.differenced(from: main)
-main = gripCornerRounder.differenced(from: main)
+struct VerticalCornerRounder: OpenSCAD {
+    var body: some OpenSCAD {
+        Union {
+            dualVerticalCornerRounder
+            Translate(0, dimensions.depth, 0) {
+                Mirror(0, 1, 0) {
+                    dualVerticalCornerRounder
+                }
+            }
+        }
+    }
+
+    var dualVerticalCornerRounder: some OpenSCAD {
+        Union {
+            singleVerticalCornerRounder
+            Translate(dimensions.width, 0, 0) {
+            Mirror(1, 0, 0) {
+                singleVerticalCornerRounder
+            }
+            }
+        }
+    }
+
+    var singleVerticalCornerRounder: some OpenSCAD {
+        Difference(parent: {
+            RectangularPrism(height: dimensions.height, width: cornerRoundingRadius, depth: cornerRoundingRadius, centered: false)
+        }) {
+            Translate(cornerRoundingRadius, cornerRoundingRadius, 0) {
+                Cylinder(height: dimensions.height, topRadius: cornerRoundingRadius, bottomRadius: cornerRoundingRadius, centered: false)
+            }
+        }
+    }
+}
+struct WidthCornerRounder: OpenSCAD {
+    var body: some OpenSCAD {
+        Translate(0, 0, dimensions.height) {
+            Rotate(0, 90, 0) {
+                Union {
+                    dualWidthCornerRounder
+                    Translate(0, dimensions.depth, 0) {
+                        Mirror(0, 1, 0) {
+                            dualWidthCornerRounder
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    var dualWidthCornerRounder: some OpenSCAD {
+        Union {
+            singleWidthCornerRounder
+            Translate(dimensions.height, 0, 0) {
+                Mirror(1, 0, 0) {
+                    singleWidthCornerRounder
+                }
+            }
+        }
+    }
+
+    var singleWidthCornerRounder: some OpenSCAD {
+        Difference(parent: {
+            RectangularPrism(height: dimensions.width, width: cornerRoundingRadius, depth: cornerRoundingRadius, centered: false)
+        }) {
+            Translate(cornerRoundingRadius, cornerRoundingRadius, 0) {
+                Cylinder(height: dimensions.width, topRadius: cornerRoundingRadius, bottomRadius: cornerRoundingRadius, centered: false)
+            }
+        }
+    }
+}
+struct DepthCornerRounder: OpenSCAD {
+    var body: some OpenSCAD {
+        Translate(0, 0, dimensions.height) {
+            Rotate(-90, 0, 0) {
+                Union {
+                    dualDepthCornerRounder
+                    Translate(0, dimensions.height, 0) {
+                        Mirror(0, 1, 0) {
+                            dualDepthCornerRounder
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    var dualDepthCornerRounder: some OpenSCAD {
+        Union {
+            singleDepthCornerRounder
+            Translate(dimensions.width, 0, 0) {
+                Mirror(1, 0, 0) {
+                    singleDepthCornerRounder
+                }
+            }
+        }
+    }
+
+    var singleDepthCornerRounder: some OpenSCAD {
+        Difference(parent: {
+            RectangularPrism(height: dimensions.depth, width: cornerRoundingRadius, depth: cornerRoundingRadius, centered: false)
+        }) {
+            Translate(cornerRoundingRadius, cornerRoundingRadius, 0) {
+                Cylinder(height: dimensions.depth, topRadius: cornerRoundingRadius, bottomRadius: cornerRoundingRadius, centered: false)
+            }
+        }
+    }
+}
+struct GripCornerRounder: OpenSCAD {
+    var body: some OpenSCAD {
+        Translate(padding + (toothpasteWidth / 2) + slopeHorizontalDistance + 1.5 - (0.125 * cornerRoundingRadius), 0, dimensions.height) {
+            Rotate(-90, 0, 0) {
+                Union {
+                    singleGripCornerRounder
+                    Translate(-3 + (0.25 * cornerRoundingRadius), 0, 0) {
+                        Mirror(1, 0, 0) {
+                            singleGripCornerRounder
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    var singleGripCornerRounder: some OpenSCAD {
+        Difference(parent: {
+            RectangularPrism(height: dimensions.depth, width: cornerRoundingRadius, depth: cornerRoundingRadius, centered: false)
+        }) {
+            Translate(cornerRoundingRadius, cornerRoundingRadius, 0) {
+                Cylinder(height: dimensions.depth, topRadius: cornerRoundingRadius, bottomRadius: cornerRoundingRadius, centered: false)
+            }
+        }
+    }
+}
+
+//OPENSCAD_print(ToothpasteSqueezer())
+print(parse(scad: ToothpasteSqueezer()))
